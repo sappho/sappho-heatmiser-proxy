ce09c253c57d69fc9a73d2e265fcf6123ddf3c28
Andrew Heald <andrew@heald.co.uk>
2012-02-15 17:08:43 +0000

    Create project bare bones, as a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SapphoHeatmiser::Application.config.secret_token = '63948ee11550ba6ea49de56cacc41ec942f8a00b169adbf584bc28637776b91f3383720c8c9a2127b816885488c27fa850cbfdcbdf6b745a3ac696cc41c59f9b'
