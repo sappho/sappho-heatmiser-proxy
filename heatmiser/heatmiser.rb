a3aeaa3e02e3ec06469fd58dcde3169def5a32e6
Andrew Heald <andrew@heald.co.uk>
2012-02-17 07:48:23 +0000

    Monitoring part is done and working.

--- heatmiser/heatmiser.rb
+++ heatmiser/heatmiser.rb
@@ -5,50 +5,66 @@ require 'socket'
 
 class Heatmiser
 
-  @lastStatus = nil
-  @commandQueue = []
-  @queueMutex = Mutex.new
-  @statusMutex = Mutex.new
-
   def initialize hostname, pin
-    @socket = TCPSocket.open(hostname, 8068)
-    @pin = pin
-    @timestamp = Time.now - 99
-    Thread.new do
-      queryCommand = addCRCToCommand [0x93, 0x0B, 0x00, @pin & 0xFF, @pin >> 8, 0x00, 0x00, 0xFF, 0xFF]
-      loop do
-        if (Time.now - @timestamp) >= 2
+    @data = {
+        :hostname => hostname,
+        :pin => pin,
+        :lastStatus => nil,
+        :timestamp => Time.now,
+        :commandQueue => [],
+        :queueMutex => Mutex.new,
+        :statusMutex => Mutex.new
+    }
+  end
+
+  def monitor
+    @thread = Thread.new @data do | data |
+      TCPSocket.open data[:hostname], 8068 do | socket |
+        queueMutex = data[:queueMutex]
+        statusMutex = data[:statusMutex]
+        commandQueue = data[:commandQueue]
+        pin = data[:pin]
+        queryCommand = addCRCToCommand [0x93, 0x0B, 0x00, pin & 0xFF, pin >> 8, 0x00, 0x00, 0xFF, 0xFF]
+        loop do
+          sleep 1
           command = queryCommand
-          @queueMutex.synchronize do
-            command = @commandQueue.shift if @commandQueue.size > 0
+          queueMutex.synchronize do
+            command = commandQueue.shift if commandQueue.size > 0
           end
-          @socket.write command
-          status = nil
           begin
+            socket.write command.pack('c*')
+            status = nil
             timeout 1 do
-              status = socket.read 81
+              status = socket.read(81).unpack('c*')
             end
+            timestamp = Time.now
             crcHi = status.pop
             crcLo = status.pop
             crc = HeatmiserCRC.new status
-            if crc.crcHi != crcHi or crc.crcLo != crcLo
-              status = nil
-            else
+            if crc.crcHi == crcHi and crc.crcLo == crcLo
               status << crcLo
               status << crcHi
+              statusMutex.synchronize do
+                data[:lastStatus] = status
+                data[:timestamp] = timestamp
+              end
             end
           rescue
-            status = nil
           end
-          @statusMutex.synchronize do
-            @lastStatus = status
-          end if status
-          puts @lastStatus
-          @timestamp = Time.now
         end
-        sleep 0.1
       end
     end.run
+  end
+
+  def sensedTemperature
+    @data[:statusMutex].synchronize do
+      status = @data[:lastStatus]
+      status && ((status[44] & 0xFF) | ((status[45] << 8) & 0xFF00)) / 10.0
+    end
+  end
+
+  def wait
+    @thread.join
   end
 
   def addCRCToCommand command
@@ -60,7 +76,9 @@ class Heatmiser
 
 end
 
-Heatmiser.new ARGV[0], Integer(ARGV[1])
+hm = Heatmiser.new(ARGV[0], Integer(ARGV[1]))
+hm.monitor
 loop do
-  sleep 0.1
+  sleep 1
+  puts hm.sensedTemperature
 end
