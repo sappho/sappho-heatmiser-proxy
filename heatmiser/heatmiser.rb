4b8e5ef28cfb890e979c6bf9c13f07b959c11a6b
Andrew Heald <andrew@heald.co.uk>
2012-02-17 14:45:49 +0000

    Try different timing and error recovery.

--- heatmiser/heatmiser.rb
+++ heatmiser/heatmiser.rb
@@ -24,46 +24,53 @@ class Heatmiser
 
   def monitor
     @thread = Thread.new @data do | data |
-      TCPSocket.open data[:hostname], 8068 do | socket |
-        queueMutex = data[:queueMutex]
-        statusMutex = data[:statusMutex]
-        commandQueue = data[:commandQueue]
-        pin = data[:pin]
-        queryCommand = [0x93, 0x0B, 0x00, pin & 0xFF, pin >> 8, 0x00, 0x00, 0xFF, 0xFF]
-        crc = HeatmiserCRC.new queryCommand
-        queryCommand << crc.crcLo
-        queryCommand << crc.crcHi
-        loop do
-          sleep 1
-          command = queryCommand
-          queueMutex.synchronize do
-            command = commandQueue.shift if commandQueue.size > 0
-          end
-          begin
-            socket.write command.pack('c*')
-            status = nil
-            timeout 1 do
-              status = socket.read(81).unpack('c*')
+      loop do
+        sleep 2
+        TCPSocket.open data[:hostname], 8068 do | socket |
+          queueMutex = data[:queueMutex]
+          statusMutex = data[:statusMutex]
+          commandQueue = data[:commandQueue]
+          pin = data[:pin]
+          queryCommand = [0x93, 0x0B, 0x00, pin & 0xFF, pin >> 8, 0x00, 0x00, 0xFF, 0xFF]
+          crc = HeatmiserCRC.new queryCommand
+          queryCommand << crc.crcLo
+          queryCommand << crc.crcHi
+          count = 0
+          loop do
+            sleep 2
+            count += 1
+            break if count > 8
+            command = queryCommand
+            queueMutex.synchronize do
+              command = commandQueue.shift if commandQueue.size > 0
             end
-            timestamp = Time.now
-            crcHi = status.pop
-            crcLo = status.pop
-            crc = HeatmiserCRC.new status
-            if (status[0] & 0xFF) == 0x94 and status[1] == 0x51 and status[2] == 0 and
-                crc.crcHi == crcHi and crc.crcLo == crcLo
-              status << crcLo
-              status << crcHi
-              statusMutex.synchronize do
-                data[:lastStatus] = {
-                    :valid => true,
-                    :raw => status,
-                    :timestamp => timestamp,
-                    :sensedTemperature => ((status[44] & 0xFF) | ((status[45] << 8) & 0x0F00)) / 10.0,
-                    :requestedTemperature => status[25] & 0xFF
-                }
+            begin
+              socket.write command.pack('c*')
+              status = nil
+              timeout 2 do
+                status = socket.read(81).unpack('c*')
               end
+              timestamp = Time.now
+              crcHi = status.pop
+              crcLo = status.pop
+              crc = HeatmiserCRC.new status
+              if (status[0] & 0xFF) == 0x94 and status[1] == 0x51 and status[2] == 0 and
+                  crc.crcHi == crcHi and crc.crcLo == crcLo
+                status << crcLo
+                status << crcHi
+                statusMutex.synchronize do
+                  data[:lastStatus] = {
+                      :valid => true,
+                      :raw => status,
+                      :timestamp => timestamp,
+                      :sensedTemperature => ((status[44] & 0xFF) | ((status[45] << 8) & 0x0F00)) / 10.0,
+                      :requestedTemperature => status[25] & 0xFF
+                  }
+                end
+                count = 0
+              end
+            rescue
             end
-          rescue
           end
         end
       end
