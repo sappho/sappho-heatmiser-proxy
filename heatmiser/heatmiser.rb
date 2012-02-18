93bb01d9787ac351a13af704e18fbf0e7f6353f8
Andrew Heald <andrew@heald.co.uk>
2012-02-18 13:33:50 +0000

    Report time since last valid reading.

--- heatmiser/heatmiser.rb
+++ heatmiser/heatmiser.rb
@@ -138,5 +138,5 @@ hm.monitor
 loop do
   sleep 5
   status = hm.lastStatus
-  puts "#{(status[:raw].collect {|byte| "%02x " % (byte & 0xFF)}).join}#{status[:requestedTemperature]} #{status[:sensedTemperature]} #{status[:heatOn]} #{status[:keyLockOn]} #{status[:frostProtectOn]} #{status[:timestamp]} #{status[:dayOfWeek]} #{status[:deviceTimeOffset]}" if status[:valid]
+  puts "#{(status[:raw].collect {|byte| "%02x " % (byte & 0xFF)}).join}#{status[:requestedTemperature]} #{status[:sensedTemperature]} #{status[:heatOn]} #{status[:keyLockOn]} #{status[:frostProtectOn]} #{status[:timeSinceLastValid]} #{status[:dayOfWeek]} #{status[:deviceTimeOffset]}" if status[:valid]
 end
