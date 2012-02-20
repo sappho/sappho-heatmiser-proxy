eb955f4a641b48160b037e06bd489dbcc48148d9
Andrew Heald <andrew@heald.co.uk>
2012-02-20 16:43:56 +0000

    Get ready for better heatmiser status validation.

--- heatmiser/heatmiser_status.rb
+++ heatmiser/heatmiser_status.rb
@@ -13,6 +13,7 @@ class HeatmiserStatus
   @mutex = Mutex.new
 
   def initialize
+    @log = TraceLog.instance
     @valid = false
     @raw = []
     @timestamp = Time.now
@@ -38,21 +39,25 @@ class HeatmiserStatus
   def set raw, timestamp, sampleTime
     @mutex.synchronize do
       @valid = true
-      @raw = raw.dup
-      @timeSinceLastValid = timestamp - @timestamp
-      @timestamp = timestamp
-      @sampleTime = sampleTime
-      @sensedTemperature = ((raw[44] & 0xFF) | ((raw[45] << 8) & 0x0F00)) / 10.0
-      @requestedTemperature = raw[25] & 0xFF
-      @heatOn = raw[47] == 1
-      @keyLockOn = raw[29] == 1
-      @frostProtectOn = raw[30] == 1
-      @deviceTimeOffset = Time.local(2000 + (raw[48] & 0xFF), raw[49], raw[50],
-                                     raw[52], raw[53], raw[54]) - timestamp
-      dayOfWeek = raw[51]
-      dayOfWeek = 0 if dayOfWeek == 7
-      @dayOfWeek = dayOfWeek
-      TraceLog.instance.info "#{(raw.collect {|byte| "%02x " % (byte & 0xFF)}).join}#{@requestedTemperature} #{@sensedTemperature} #{@heatOn} #{@keyLockOn} #{@frostProtectOn} #{@timeSinceLastValid} #{@dayOfWeek} #{@deviceTimeOffset} #{sampleTime}"
+      begin
+        @raw = raw.dup
+        @sensedTemperature = ((raw[44] & 0xFF) | ((raw[45] << 8) & 0x0F00)) / 10.0
+        @requestedTemperature = raw[25] & 0xFF
+        @heatOn = raw[47] == 1
+        @keyLockOn = raw[29] == 1
+        @frostProtectOn = raw[30] == 1
+        @deviceTimeOffset = Time.local(2000 + (raw[48] & 0xFF), raw[49], raw[50],
+                                       raw[52], raw[53], raw[54]) - timestamp
+        dayOfWeek = raw[51]
+        @dayOfWeek = dayOfWeek == 7 ? 0 : dayOfWeek
+        @timeSinceLastValid = timestamp - @timestamp
+        @timestamp = timestamp
+        @sampleTime = sampleTime
+        @log.info "#{(raw.collect {|byte| "%02x " % (byte & 0xFF)}).join}#{@requestedTemperature} #{@sensedTemperature} #{@heatOn} #{@keyLockOn} #{@frostProtectOn} #{@timeSinceLastValid} #{@dayOfWeek} #{@deviceTimeOffset} #{sampleTime}"
+      rescue => error
+        @log.error error
+        @valid = false
+      end
     end
   end
 
