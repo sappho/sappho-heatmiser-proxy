1bbba2d60c9486a0cc6bee8dd017202474ded386
Andrew Heald <andrew@heald.co.uk>
2012-02-21 11:34:27 +0000

    Create a client register, enforce max clients, and log better. (reverse-merged from commit d7327329352ee1fe494bb8a4f7581c1370eb6865) This didn't work, so needs a bug fix session.

--- heatmiser/heatmiser_proxy.rb
+++ heatmiser/heatmiser_proxy.rb
@@ -1,5 +1,4 @@
 require 'heatmiser_client'
-require 'client_register'
 require 'thread'
 require 'socket'
 require 'trace_log'
@@ -13,17 +12,9 @@ class HeatmiserProxy
       log.info "opening proxy server port #{port}"
       TCPServer.open port do | server |
         log.info "proxy server port #{port} is now open"
-        clients = ClientRegister.instance
         loop do
           log.info "listening for new clients on proxy server port #{port}"
-          client = server.accept
-          begin
-            clients.put client
-            HeatmiserClient.new.session client
-          rescue => error
-            log.error error
-          end
-          clients.close client
+          HeatmiserClient.new(server.accept).session
         end
       end
     end
