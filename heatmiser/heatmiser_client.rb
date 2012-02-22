797e43f6e28059050243b521d6d2c0537563da84
Andrew Heald <andrew@heald.co.uk>
2012-02-22 10:19:40 +0000

    Harden the code a bit.

--- heatmiser/heatmiser_client.rb
+++ heatmiser/heatmiser_client.rb
@@ -15,28 +15,28 @@ class HeatmiserClient
     @clients.register client
     @ip = @clients.ip client
     @client = client
+    @status = HeatmiserStatus.instance
+    @log = TraceLog.instance
     Thread.new do
-      status = HeatmiserStatus.instance
-      log = TraceLog.instance
       loop do
         begin
           timeout 20 do
             command = read 5
-            log.debug "header: #{TraceLog.hex command}" if log.debug?
+            @log.debug "header: #{TraceLog.hex command}" if @log.debug?
             packetSize = (command[1] & 0xFF) | ((command[2] << 8) & 0x0F00)
             command += read(packetSize - 5)
             CommandQueue.instance.push @ip, command unless (command[0] & 0xFF) == 0x93
-            status.get { @client.write status.raw.pack('c*') if status.valid }
-            log.info "command received from client #{@ip} so it is alive"
+            @status.get { @client.write @status.raw.pack('c*') if @status.valid }
+            @log.info "command received from client #{@ip} so it is alive"
           end
         rescue Timeout::Error
-          log.info "no command received from client #{@ip} so presuming it dormant"
+          @log.info "no command received from client #{@ip} so presuming it dormant"
           break
         rescue ReadError
-          log.info "unable to receive data from client #{@ip} so presuming it has disconnected"
+          @log.info "unable to receive data from client #{@ip} so presuming it has disconnected"
           break
         rescue => error
-          log.error error
+          @log.error error
           break
         end
       end
