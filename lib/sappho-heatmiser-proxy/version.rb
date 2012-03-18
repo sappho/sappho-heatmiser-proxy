413a617db25c84260c73dd3c1fd1fb50f30e983d
Andrew Heald <andrew@heald.co.uk>
2012-03-18 23:09:16 +0000

    Fix dependency.

--- lib/sappho-heatmiser-proxy/version.rb
+++ lib/sappho-heatmiser-proxy/version.rb
@@ -7,7 +7,7 @@ module Sappho
   module Heatmiser
     module Proxy
       NAME = 'sappho-heatmiser-proxy'
-      VERSION = '0.1.0'
+      VERSION = '0.1.1'
       AUTHORS = ['Andrew Heald']
       EMAILS = ['andrew@heald.co.uk']
       HOMEPAGE = 'https://github.com/sappho/sappho-heatmiser-proxy/wiki'
