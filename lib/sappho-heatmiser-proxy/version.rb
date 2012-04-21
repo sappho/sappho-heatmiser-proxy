05a704b596f59a11ad43db8ee5df3c7d47de9dd8
Andrew Heald <andrew@heald.co.uk>
2012-04-21 09:27:44 +0100

    Constantly sample while there's at least one connected client.

--- lib/sappho-heatmiser-proxy/version.rb
+++ lib/sappho-heatmiser-proxy/version.rb
@@ -7,7 +7,7 @@ module Sappho
   module Heatmiser
     module Proxy
       NAME = 'sappho-heatmiser-proxy'
-      VERSION = '0.1.8'
+      VERSION = '0.1.9'
       AUTHORS = ['Andrew Heald']
       EMAILS = ['andrew@heald.co.uk']
       HOMEPAGE = 'https://github.com/sappho/sappho-heatmiser-proxy/wiki'
